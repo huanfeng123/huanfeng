a4ea96688ee1457ebfe68aeb33a7cc0c7accadc5
huanfeng123 <56348872+huanfeng123@users.noreply.github.com>
2023-03-08 23:03:31 +0800

    Update and rename src/default.ts to 来源/default.ts

--- 来源/default.ts
+++ 来源/default.ts
@@ -7,7 +7,7 @@ export const defaultSetting = {
 }
 
 export const defaultMessage = `
-- 本站仅用于演示，填入自己的 key 才可使用。
+- 本站仅用于演示，填入自己的 key 才可使用555。
 - 由 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 和 [Vercel](http://vercel.com/) 提供支持。
 - 由 [ourongxing](https://github.com/ourongxing) 修改自 [Diu](https://github.com/ddiu8081/chatgpt-demo)，查看 [源码](https://github.com/ourongxing/chatgpt-vercel)，欢迎自部署。
 - **Shift+Enter** 换行。开头输入 **/** 或者 **空格** 搜索 Prompt 预设。点击输入框滚动到底部。`
