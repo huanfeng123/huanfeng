3088a31f831327bb425df76f72864d4353a2c49b
huanfeng123 <56348872+huanfeng123@users.noreply.github.com>
2023-03-08 23:27:32 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -7,6 +7,6 @@ export const defaultSetting = {
 }
 
 export const defaultMessage = `
-- 本站仅用于演示，填入自己的 key 才可5555使用。
+- 本站仅用于演示，填入自己的 key 才可使用。
 - 由 [是枭](https://blog.evv1.com) 搭建功能强大，响应速度快，欢迎自部署。
 - <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
