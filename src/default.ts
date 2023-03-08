a1ed43c93968bd67a496b38e2f93bbe26789e94a
huanfeng123 <56348872+huanfeng123@users.noreply.github.com>
2023-03-08 23:25:00 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -8,6 +8,5 @@ export const defaultSetting = {
 
 export const defaultMessage = `
 - 本站仅用于演示，填入自己的 key 才可5555使用。
-- 由 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 和 [Vercel](http://vercel.com/) 提供支持。
-- 由 [ourongxing](https://github.com/ourongxing) 修改自 [Diu](https://github.com/ddiu8081/chatgpt-demo)，查看 [源码](https://github.com/ourongxing/chatgpt-vercel)，欢迎自部署。
+- 由 [是枭](https://blog.evv1.com) 搭建功能强大，响应速度快，欢迎自部署。
 - <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
