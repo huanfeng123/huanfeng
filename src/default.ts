2fc37d959cb23b63673c1fd4720f446214656987
huanfeng123 <56348872+huanfeng123@users.noreply.github.com>
2023-04-04 01:19:14 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -7,7 +7,8 @@ export const defaultSetting = {
 }
 
 export const defaultMessage = `
-- 本站仅用于演示，填入自己的 key 才可使用。
+- 本站仅用于演示，填入自己的 key 才可使用。现在免费镜像站点越来越少，是因为ChatGPT封号太猛的原因，导致所有镜像可能都不稳定。
+- 此软件和镜像都是由 [枭]收集而来，不保证稳定性，如果你想稳定的使用即可购买属于自己的ChatGPT账号，[购买地址点击我即可](http://fk.119.cab) 。
 - 由 [是枭](https://blog.evv1.com) 搭建功能强大，响应速度快，欢迎自部署。
 -  [自由国度](https://bbs.119.cab) 推荐此论坛，更多ChatGPT教程，还有各种数据资源欢迎您的到来！
 - <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
